2c83291c58627cd68bdb7ba01d9b2a4e470b2d9d
Robbie <rob76c@gmail.com>
2024-10-06 01:03:03 -0400

    Fetch one Post

--- WeChangePosts/app/(drawer)/(tabs)/feed/post/[id].tsx
+++ WeChangePosts/app/(drawer)/(tabs)/feed/post/[id].tsx
@@ -1,18 +1,25 @@
-import posts from "@/assets/data/posts";
 import Post from "@/components/Post";
 import { useGlobalSearchParams } from "expo-router";
-import { Text } from "react-native";
-
+import { ActivityIndicator, Text } from "react-native";
+import { useQuery } from "@tanstack/react-query";
+import { getPost } from "@/lib/api/posts";
 
 export default function PostScreen () {
     const {id} = useGlobalSearchParams();
 
-    const post= posts.find((t) => t.id === id);
+    const {data, isLoading, error} = useQuery ({
+        queryKey: ['post',id],
+        queryFn: () => getPost(id as string)
+    })
 
+    if (isLoading) {
+        return <ActivityIndicator />;
 
-    if (!post) {
-        return <Text> Post {id} not found!</Text>;
     }
 
-    return <Post post = {post} />;
+    if (error) {
+        return <Text> Post {id} not found!</Text>
+    }
+
+    return <Post post = {data} />;
 }
--- WeChangePosts/lib/api/posts.ts
+++ WeChangePosts/lib/api/posts.ts
@@ -7,8 +7,29 @@ export const listPosts= async () => {
          
         },
       });
+      if (res.status== 400) {
+        throw new Error('Not authorized, please sign in')
+      }
       if (res.status !== 200) {
         throw new Error ("Error fetching posts!");
       }
       return await res.json();
 };
+
+export const getPost = async (id:string) => {
+    const res = await fetch(`${API_URL}/post/ ${id}`, {
+        headers: {
+          Authorization:`Bearer ${authToken}`,
+         
+        },
+      });
+      if (res.status== 400) {
+        throw new Error('Not authorized, please sign in')
+      }
+      if (res.status !== 200) {
+        throw new Error ("Error fetching posts!");
+      }
+      return await res.json();
+
+
+}
